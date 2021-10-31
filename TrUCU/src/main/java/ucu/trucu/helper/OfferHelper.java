8ce552284f3280709655b4d7b79265e2aae9de61
Seba Mazzey <mazzeyseba@gmail.com>
2021-10-30 22:18:27 -0300

    Add/Delete Offer

--- TrUCU/src/main/java/ucu/trucu/helper/OfferHelper.java
+++ TrUCU/src/main/java/ucu/trucu/helper/OfferHelper.java
@@ -1,9 +1,37 @@
 package ucu.trucu.helper;
 
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ucu.trucu.model.dao.OfferDAO;
+import ucu.trucu.model.dto.Offer;
+
 /**
  *
  * @author NicoPuig
  */
+@Service
 public class OfferHelper {
+    @Autowired
+    private OfferDAO offerDAO;
     
+    public void createOffer(Offer newOffer, List<Integer> idPublications) throws SQLException {
+        offerDAO.insert(newOffer);
+        for(int idPublication: idPublications) {
+            offerDAO.addOfferedPublications(newOffer, idPublication);
+        }
+    }
+    
+    public void deleteOffer(int idOffer) throws SQLException {
+        // Elimino las publicaciones relacionadas con la oferta
+        offerDAO.deleteOfferedPublications(idOffer, where -> where.and(
+                where.eq("idOffer", idOffer))
+        );
+        
+        // Elimino la oferta
+        offerDAO.delete(where -> where.and(
+                where.eq("idOffer", idOffer))
+        );
+    }
 }
--- TrUCU/src/main/java/ucu/trucu/model/dao/OfferDAO.java
+++ TrUCU/src/main/java/ucu/trucu/model/dao/OfferDAO.java
@@ -1,7 +1,15 @@
 package ucu.trucu.model.dao;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 import org.springframework.stereotype.Component;
+import ucu.trucu.database.querybuilder.Filter;
+import ucu.trucu.database.querybuilder.QueryBuilder;
 import ucu.trucu.model.dto.Offer;
+import ucu.trucu.model.dto.Publication;
+import ucu.trucu.util.DBUtils;
 
 /**
  *
@@ -23,5 +31,25 @@ public class OfferDAO extends AbstractDAO<Offer> {
     @Override
     public Offer findByPK(String... primaryKeys) {
         return findFirst(where -> where.eq("idOffer", primaryKeys[0]));
+    }
+    
+    public void addOfferedPublications(Offer offer, int idPublication) throws SQLException {
+        // Creo una oferta y una publicación unicamente con los ID
+        Offer offerID = new Offer();
+        offerID.setIdOffer(offer.getIdOffer());
+        Publication publication = new Publication();
+        publication.setIdPublication(idPublication);
+        dbController.executeStatement(
+                QueryBuilder.insertInto("OfferedPublications")
+                        .keyValue(DBUtils.objectToPropertyMap(publication))
+                        .keyValue(DBUtils.objectToPropertyMap(offerID))
+        );
+    }
+    
+    public void deleteOfferedPublications(int idOffer, Function<Filter, String> filter) throws SQLException {
+        dbController.executeStatement(
+                QueryBuilder.deleteFrom("OfferedPublications")
+                        .where(filter)
+        );
     }
 }
