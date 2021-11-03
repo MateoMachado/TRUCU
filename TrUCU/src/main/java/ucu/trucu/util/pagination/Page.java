3cfb1b2d1fc9d20c7ac12dd75048252de0db3f18
NicolasPuig <nicopuig14@hotmail.com>
2021-11-03 00:33:46 -0300

    Fix Pagination

--- TrUCU/src/main/java/ucu/trucu/util/pagination/Page.java
+++ TrUCU/src/main/java/ucu/trucu/util/pagination/Page.java
@@ -9,16 +9,22 @@ import java.util.List;
  */
 public class Page<T> {
 
+    private final int totalElements;
     private final int totalPages;
     private final int pageNumber;
     private final int pageSize;
     private final List<T> content;
 
-    public Page(int totalPages, int pageNumber, int pageSize, List<T> content) {
-        this.totalPages = totalPages;
+    public Page(int totalElements, int pageNumber, int pageSize, List<T> content) {
+        this.totalElements = totalElements;
         this.pageNumber = pageNumber;
         this.pageSize = pageSize == 0 ? content.size() : pageSize;
         this.content = content;
+        this.totalPages = this.pageSize == 0 ? 0 : (int) Math.ceil((float) this.totalElements / this.pageSize);
+    }
+
+    public int getTotalElements() {
+        return totalElements;
     }
 
     public int getTotalPages() {
