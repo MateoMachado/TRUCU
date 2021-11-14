0ef0d92d0edc66e7662fc82381c5808494ad6d6b
Seba Mazzey <mazzeyseba@gmail.com>
2021-11-14 00:15:12 -0300

    import missing

--- TrUCU/src/main/java/ucu/trucu/api/ReportController.java
+++ TrUCU/src/main/java/ucu/trucu/api/ReportController.java
@@ -1,6 +1,7 @@
 package ucu.trucu.api;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
